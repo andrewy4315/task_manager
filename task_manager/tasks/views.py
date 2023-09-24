f1f7c334770a355fae9ecfe3b58ad0ddd1cdb0d0
andrewy4315 <andrewy4315@gmail.com>
2023-09-23 21:15:11 -0400

    Initial commit

---
+++ task_manager/tasks/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+@login_required
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/create_task.html', {'form': form})
+
+@login_required
+def complete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
